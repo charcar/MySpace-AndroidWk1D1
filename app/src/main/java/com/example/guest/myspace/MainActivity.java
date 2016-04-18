b1484c18fcc9802ed4dbe76c94a99b38f121b642
charles carter and tyler suderman <tcsuder@gmail.com>
2016-04-18 16:06:35 -0700

    add new pics activity

--- app/src/main/java/com/example/guest/myspace/MainActivity.java
+++ app/src/main/java/com/example/guest/myspace/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.guest.myspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mbuttonTomPictures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbuttonTomPictures = (Button) findViewById(R.id.buttonTomPictures);
+        mbuttonTomPictures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TomsPics.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/guest/myspace/TomsPics.java
@@ -0,0 +1,13 @@
+package com.example.guest.myspace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TomsPics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toms_pics);
+    }
+}
